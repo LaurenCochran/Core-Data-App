d8849eb193d784d98ae98d901f5ba81c17a22cf6
Lauren <Owner1@MacBooks-MacBook-Pro.local>
2019-04-19 21:15:49 -0500

    First

---
+++ ToDo/ToDoViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Lauren on 2019-04-19.
+//  Copyright © 2019 Lauren Cochran. All rights reserved.
+//
+
+import UIKit
+
+class ToDoViewController: UIViewController {
+    @IBOutlet weak var toDoText: UITextField!
+    
+    @IBOutlet weak var descriptionText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
